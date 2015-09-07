04488a69efec46ff1ef7869bd3ae432383e06491
julio <julio336@hotmail.com>
2015-09-07 00:12:17 -0500

    first commit

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    var arr:[String] = ["frame1.jpg,", "frame2.jpg", "frame3.jpg", "frame4.jpg", "frame5.jpg"]
+    var i = 1
+    var timer = NSTimer()
+    var counter = 1
+
+
+    @IBOutlet var animatedImage: UIImageView!
+
+    @IBOutlet var buttonAnimate: UIButton!
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        if counter == 1 {
+            stopAnimation()
+            counter++
+        } else {
+            activeAnimation()
+            counter = 1
+        }
+        
+        
+    }
+    
+    func stopAnimation(){
+        timer.invalidate()
+        buttonAnimate.setTitle("Animate", forState: .Normal)
+
+    }
+    
+    func activeAnimation(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "doAnimation", userInfo: nil, repeats: true)
+        buttonAnimate.setTitle("Stop", forState: .Normal)
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "doAnimation", userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    func doAnimation(){
+        if i == 5 {
+            i = 1
+        } else {
+            animatedImage.image = UIImage(named: arr[i])
+            i++
+
+        }
+      
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +68,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+/*
+    override func viewDidLayoutSubviews() {
+        animatedImage.center = CGPointMake(animatedImage.center.x - 400, animatedImage.center.y)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            
+            self.animatedImage.center = CGPointMake(self.animatedImage.center.x + 400, self.animatedImage.center.y)
+            
+        })
+        
+    }
+  */
+    
+    
 }
 
